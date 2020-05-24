0aa4ff497a8e630bd3bea4f75dd5fa4ad6a8f61c
juguemseok <kalkin2@naver.com>
2020-05-24 17:12:14 +0900

    LOGIC CHANGE

--- src/Step1.java
+++ src/Step1.java
@@ -1,7 +1,14 @@
+import com.sun.corba.se.spi.ior.IdentifiableContainerBase;
+
 /**
  A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 
- For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+ For example, number 9 has binary representation 1001 and contains a binary gap of length 2.
+
+ The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3.
+ The number 20 has binary representation 10100 and contains one binary gap of length 1.
+ The number 15 has binary representation 1111 and has no binary gaps.
+ The number 32 has binary representation 100000 and has no binary gaps.
 
  Write a function:
 
@@ -9,7 +16,9 @@
 
  that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
 
- For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+ For example,
+ given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
+ Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
 
  Write an efficient algorithm for the following assumptions:
 
@@ -21,29 +30,23 @@ class Step1 {
     public static int binaryTest(int N) {
         // write your code in Java SE 8
         String result = Integer.toBinaryString(N);
+        char[] resultChar = result.toCharArray();
         int maxBinaryLen = 0;
 
-        int firstLen = 0;
-        int secondLen = 0;
-
-        //첫번째 1을 찾는다. 
-        for(int i = 0 ; i< result.length(); i++){
-            if(result.substring(i,i+1).equals("1")){
-                firstLen = i;
-                for( int j = i+1 ; j < result.length() ; j++){
-                    if(result.substring(j,j+1).equals("1")){
-                        secondLen = j;
-                        if(maxBinaryLen <  (secondLen - firstLen)){
-                            maxBinaryLen =  (secondLen - firstLen)-1  ;
-                            i = i+1;
-                            break;
-                        }
-                    }
+        //SECOND LOGIC
+        int gapLen = 0;//사이값
+        for(int i = 0; i < result.length() ; i++){
+            //0일때 값 증가
+            if(resultChar[i]=='0'){
+                gapLen++;
+            }else{
+                //1을 만났을때만 최대값을 바꿔준다. 100100000 이런경우를 대비해서..
+                if(gapLen>maxBinaryLen){
+                    maxBinaryLen = gapLen;
                 }
+                gapLen = 0;
             }
-
         }
-
         return maxBinaryLen;
     }
 
